bf34d7ddeafd0f43c40edd5ba50c943a48bca9d2
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-09-17 23:25:33 +0530

    added blogs and made blog post page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    sno = models.IntegerField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    timeStamp = models.DateTimeField(blank=True)
+    slug = models.CharField(max_length=200)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from blog.models import Post
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    everypost = Post.objects.all()
+    return render(request, 'blog/home.html', {'everypost': everypost})
 
 
 def blogpost(request, slug):
-    return render(request, 'blog/post.html')
+    post = Post.objects.filter(slug=slug).first()
+    return render(request, 'blog/post.html', {'post': post})
